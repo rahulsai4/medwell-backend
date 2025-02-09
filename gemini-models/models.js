397c1754a7ef9261429ac800eb995c3ca658cc23
rahul <rahulsai2620@gmail.com>
2025-02-09 13:07:49 +0530

    global models

--- gemini-models/models.js
+++ gemini-models/models.js
@@ -1,6 +1,12 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
+const generalInstr = `
+if the user has provided somethings completely irrelevent information just ignore it and point the user to provide relevant info only and make sure the reply is small
+always provide me minimal but crucial info only.
+make sure to not skip the crucial info.
+`;
+const 
 const diagnosisModel = genAI.getGenerativeModel({ 
     model: "gemini-1.5-flash", 
     systemInstruction: 
@@ -8,7 +14,8 @@ const diagnosisModel = genAI.getGenerativeModel({
             You are an expert doctor specializing in diagnosing symptoms and providing evidence-based health advice.
         Based on the following symptoms, suggest possible causes, next steps for evaluation, dietary adjustments, and lifestyle tips:
 
-        if the user has provided somethings completely irrelevent information just ignore it and point the user to provide relevant info only and make sure the reply is small
+        ${generalInstr}
+        
 
         ### Response Guidelines:
         - Possible Causes: List potential conditions or factors (e.g., dehydration, stress, infections).
@@ -23,7 +30,7 @@ const dietModel = genAI.getGenerativeModel({
     `You are an expert nutritionist specializing in creating personalized diet plans.
     Based on the following user inputs, provide a well-balanced, customized meal plan and dietary advice:
 
-    if the user has provided somethings completely irrelevent information just ignore it and point the user to provide relevant info only and make sure the reply is small
+    ${generalInstr}
 
     ### Response Guidelines:
     - Meal Plan: Provide a 7-day meal plan with breakfast, lunch, dinner, and snacks.
@@ -40,7 +47,8 @@ const fitnessModel = genAI.getGenerativeModel({
             You are an expert fitness coach specializing in designing personalized exercise routines and wellness strategies.
         Based on the following user inputs, provide a tailored fitness plan and general wellness advice:
         
-        if the user has provided somethings completely irrelevent information just ignore it and point the user to provide relevant info only and make sure the reply is small
+        ${generalInstr}
+
         ### Response Guidelines:
         - Workout Plan: Design a weekly workout plan with specific exercises (e.g., cardio, strength training, flexibility).
         - Warm-Up/Cool-Down: Include warm-up and cool-down routines.
